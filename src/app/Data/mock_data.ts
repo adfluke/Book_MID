e35ea72ab2243255b96b1546ee1b6d426ead4dfe
s6204062630637 <s6204062630637@email.kmutnb.ac.th>
2022-03-05 21:22:29 +0700

    lastnew

--- src/app/Data/mock_data.ts
+++ src/app/Data/mock_data.ts
@@ -3,14 +3,14 @@ export class mockFlightData {
   public static mfData: Flight[] = [
     {
       fullName: 'adun piwgram',
-      from: 'JAPAN',
-      to: 'TH',
+      from: 'TH',
+      to: 'USA',
       type: 'return',
       adults: 1,
-      departure: new Date('2565-03-1'),
+      departure: new Date('2565-03-5'),
       children: 2,
       infants: 3,
-      arrival: new Date('2565-03-2'),
+      arrival: new Date('2565-03-6'),
     },
   ];
 }
